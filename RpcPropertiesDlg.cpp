02ce286d36454c6927db1e27b32de7a3aa5b19d6
Diana Kalinichenko <dkalinichenko@amcbridge.com>
2019-11-01 16:09:27 +0200

    RPCR6-45 Revert last changes

--- RpcPropertiesDlg.cpp
+++ RpcPropertiesDlg.cpp
@@ -212,7 +212,6 @@ void CRpcPropertiesDlg::CreateRpcUI(bool bMultipleSelection)
 	if (bMultipleSelection)
 	{
 		m_btMassEditButton.ShowWindow(SW_SHOW);
-		m_rcRpcUiWnd = HackRpcUiRect();
 	}
 	else
 	{
